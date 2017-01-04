8cd64b4e51c79b5ee1eff685c7dcee55e8544860
Mohamad Zabad <zabadmohamad@yahoo.com>
2017-01-04 21:26:03 +0000

    Little update to the to do list

---
+++ Password_Manager_v2.0.c
@@ -0,0 +1,24 @@
+/*
+    Mohamad Zabad
+    
+    Password_Manager_v2.0
+        This program will safely save  all the passwords that you enter.
+*/
+
+/*
+    TO DO LIST:
+        - Create the interface
+        - add the key stroke functions
+        - make the login into the program
+        - make sure that the program creates all the files needed
+        - add te functions for each option
+        - More to come...
+*/
+
+main()
+{
+    int option_index;
+    
+    
+    
+}
