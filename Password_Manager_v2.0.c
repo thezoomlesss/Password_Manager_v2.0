1c7a398280d128c8c46e40479506af1eb4e1efbf
Mohamad Zabad <zabadmohamad@yahoo.com>
2017-03-03 15:01:37 +0000

    New input detection
    Now recognising the arrow keys for the menu
    Now the index resets when it reaches either the higher or lower limit

--- Password_Manager_v2.0.c
+++ Password_Manager_v2.0.c
@@ -15,57 +15,94 @@
         - More to come...
 */
 #include <stdio.h>
+#include <stdlib.h> // For the delay()
 #include <windows.h> // used for system("CLS")    could become useful
-#include <conio.h>
+#include <conio.h> // for colours
 
-void menu(char state);
+void menu(int state);
 
-char key;
     
 
 main()
 {
+    char ch;
+    int menu_index=0;
     
+    menu(0);
     while(1)
     {
-        if( kbhit() )
+        if(kbhit())
         {
-            key=getch();
-            menu(key);
-        }
-    }
+            /*
+                                We take in the input and check if it's either UP or DOWN arrow key
+                                If it's one of them then we increment/decrement
+                        */
+            
+            ch = getch();
+            if(ch==72 && menu_index < 4)  // UP key
+            {
+                menu_index++;
+            }
+            else if(ch==80 && menu_index>-1)  // else if DOWN key
+            {
+                menu_index--;
+            }
+            if(menu_index > 3)
+            {
+                menu_index=0;
+            }
+            else if( menu_index < 0 )
+            {
+                menu_index=3;
+            }
+            menu(menu_index);
+            Sleep(80);
+        } // end if kbhit()
+    } // end while
 }
 
-void menu(char state)
+void menu(int state)
 {
     
     clrscr();
     printf("\t\t\t\t\tMenu\n\n");
     switch(state)
     {
-        case '0':
+        case 0:
         {
-            printf("\t\t\t\t\tMenu1\n");
+            textcolor(RED);
+            printf("\t\t\t\t\t");
+            cprintf("Enter Record");
+            printf("\n");
             break;
         }
-        case '1':
+        case 1:
         {
-            printf("\t\t\t\t\tMenu2\n");
+            textcolor(RED);
+            printf("\t\t\t\t\t");
+            cprintf("Display Record");
+            printf("\n");
             break;
         }
-        case '2':
+        case 2:
         {
-            
+            textcolor(RED);
+            printf("\t\t\t\t\t");
+            cprintf("Test2");
+            printf("\n");
             break;
         }
-        case '3':
+        case 3:
         {
-            
+            textcolor(RED);
+            printf("\t\t\t\t\t");
+            cprintf("Test3");
+            printf("\n");
             break;
         }
         default:
         {
-            printf("\t\t\t\t\tTest\n");
+            printf("TEST");
         }
     } // end switch
 } // end menu()
