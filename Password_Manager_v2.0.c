4ffa1901ad66e651b90530bafd2a69f32a557d3e
Mohamad Zabad <zabadmohamad@yahoo.com>
2017-03-06 10:32:15 +0000

    Added an interface + a struct for the record

--- Password_Manager_v2.0.c
+++ Password_Manager_v2.0.c
@@ -19,7 +19,19 @@
 #include <windows.h> // used for system("CLS")    could become useful
 #include <conio.h> // for colours
 
+// Structs
+struct record
+{
+    char title[25];
+    char username[30];
+    char password[30];
+    char email[35];
+    char notes[50];
+};
+
+// Function Protorypes
 void menu(int state);
+int enter_record(void);
 
     
 
@@ -41,11 +53,11 @@ main()
             ch = getch();
             if(ch==72 && menu_index < 4)  // UP key
             {
-                menu_index++;
+                menu_index--;
             }
             else if(ch==80 && menu_index>-1)  // else if DOWN key
             {
-                menu_index--;
+                menu_index++;
             }
             if(menu_index > 3)
             {
@@ -56,7 +68,7 @@ main()
                 menu_index=3;
             }
             menu(menu_index);
-            Sleep(80);
+            Sleep(40);
         } // end if kbhit()
     } // end while
 }
@@ -70,31 +82,84 @@ void menu(int state)
     {
         case 0:
         {
-            textcolor(RED);
+            textcolor(LIGHTGREEN);
             printf("\t\t\t\t\t");
             cprintf("Enter Record");
+            printf("\n");
+            
+            textcolor(LIGHTRED);
+            printf("\t\t\t\t\t");
+            cprintf("Display Record");
+            printf("\n");
+            printf("\t\t\t\t\t");
+            cprintf("Test2");
+            printf("\n");
+            printf("\t\t\t\t\t");
+            cprintf("Test3");
             printf("\n");
             break;
         }
         case 1:
         {
-            textcolor(RED);
+            textcolor(LIGHTRED);
+            printf("\t\t\t\t\t");
+            cprintf("Enter Record");
+            printf("\n");
+            
+            
+            textcolor(LIGHTGREEN);
             printf("\t\t\t\t\t");
             cprintf("Display Record");
             printf("\n");
+            
+            textcolor(LIGHTRED);
+            printf("\t\t\t\t\t");
+            cprintf("Test2");
+            printf("\n");
+            printf("\t\t\t\t\t");
+            cprintf("Test3");
+            printf("\n");
+            
             break;
         }
         case 2:
         {
-            textcolor(RED);
+            textcolor(LIGHTRED);
+            printf("\t\t\t\t\t");
+            cprintf("Enter Record");
+            printf("\n");
+            printf("\t\t\t\t\t");
+            cprintf("Display Record");
+            printf("\n");
+            
+            
+            textcolor(LIGHTGREEN);
             printf("\t\t\t\t\t");
             cprintf("Test2");
             printf("\n");
+            
+            textcolor(LIGHTRED);
+            printf("\t\t\t\t\t");
+            cprintf("Test3");
+            printf("\n");
+           
             break;
         }
         case 3:
         {
-            textcolor(RED);
+            textcolor(LIGHTRED);
+            printf("\t\t\t\t\t");
+            cprintf("Enter Record");
+            printf("\n");
+            printf("\t\t\t\t\t");
+            cprintf("Display Record");
+            printf("\n");
+            printf("\t\t\t\t\t");
+            cprintf("Test2");
+            printf("\n");
+            
+            
+            textcolor(LIGHTGREEN);
             printf("\t\t\t\t\t");
             cprintf("Test3");
             printf("\n");
@@ -106,3 +171,11 @@ void menu(int state)
         }
     } // end switch
 } // end menu()
+
+int menu(void)
+{
+    struct record a;
+    
+    
+    return(1);
+}
