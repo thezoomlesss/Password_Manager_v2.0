f419f461e2e4c0199abe44d2de5c4bc3c341da9a
Mohamad Zabad <zabadmohamad@yahoo.com>
2017-03-07 12:48:12 +0000

    Code refactoring + Login process started

--- Password_Manager_v2.0.c
+++ Password_Manager_v2.0.c
@@ -34,10 +34,11 @@ struct record
 };
 
 // Function Protorypes
-void menu(int state);   // Main menu
-void menu1(int state);  // Login/Register menu
-void menu_input(void);
+void menu1(int state, int condition);  // Login/Register menu
 void menu1_input(void);
+void menu1_select(int state);
+void menu(int state);   // Main menu
+void menu_input(void);
 int enter_record(void);
 
     
@@ -47,7 +48,7 @@ main()
     menu1_input();
     menu_input();
 }
-void menu1(int state)
+void menu1(int state, int condition)
 {
     clrscr();
     switch(state)
@@ -113,7 +114,125 @@ void menu1(int state)
             printf("Bug %d", state);
         }
     }// end switch
+    
+    if( condition ) menu1_select(state);
 } // end void menu1
+void menu1_input(void)
+{
+    char ch;
+    int menu_index=0;
+    
+    /*
+                The Login menu
+        */
+    menu1(menu_index,0);
+    while(1)
+    {
+        if(kbhit())
+        {
+            /*
+                                We take in the input and check if it's either UP or DOWN arrow key
+                                If it's one of them then we increment/decrement
+                        */
+            ch=getch();
+            if(ch==72 && menu_index > -1)  // UP key
+            {
+                menu_index--;
+            }
+            else if(ch==80 && menu_index < 3)  // else if DOWN key
+            {
+                menu_index++;
+            }
+            
+            if(menu_index > 2)
+            {
+                menu_index=0;
+            }
+            else if(menu_index < 0)
+            {
+                menu_index=2;
+            }
+            
+            // If the user pressed enter
+            if(ch == 13) 
+            {
+                menu1(menu_index,1);
+            }
+            menu1(menu_index, 0);
+            Sleep(40);
+        } // end kbhit()
+    } // end while() 
+} // end menu1_input
+void menu1_select(int state)
+{
+    FILE *fp1;
+    char username[20];
+    
+    switch(state)
+    {
+        case 0:
+        {
+            do
+            {
+                clrscr();
+                printf("\n\n\t\t\t\t\t\t");
+                textcolor(LIGHTCYAN);
+                cprintf("Log In");
+                
+                textcolor(LIGHTGREEN);
+                printf("\n\n\t\t\t\t\t");
+                cprintf("Username"); 
+                textcolor(WHITE);
+                cprintf(" (max 20)");
+                textcolor(LIGHTRED);
+                cprintf(": ");
+                scanf("%s", username);
+                flushall();
+                
+                if(strlen( username ) > 19 )
+                {
+                    do
+                    {    
+                        clrscr();
+                        printf("\n\n\t\t\t\t\t\t");
+                        textcolor(LIGHTCYAN);
+                        cprintf("Log In");
+                        
+                        textcolor(LIGHTRED);
+                        printf("\n\n\t\t\t\t");
+                        cprintf("The username can't be longer than 20 characters!");
+                        textcolor(LIGHTGREEN);
+                        printf("\n\t\t\t\t\t");
+                        cprintf("Username: ");
+                        scanf("%s", username);
+                        flushall();
+                    } while(strlen( username ) > 19 );
+                } // end if username > 19 chars
+            } while(strlen( username ) > 19 );
+            break;
+        }
+        
+        case 1:
+        {
+            // register new name and create file
+            break;
+        }
+        
+        case 3:
+        {
+            exit(0);
+        }
+        
+        default :
+        {
+            printf("Bug");
+        }
+    } // end switch
+    
+    
+    
+} // end menu1_select
+
 void menu(int state)
 {
     
@@ -213,7 +332,7 @@ void menu(int state)
     } // end switch
 } // end menu()
 
-void menu_input(void)      // This should get passed the user string as a parameter
+void menu_input(void)      // This should get passed the user string as a parameter once we know who the user is
 {
     char ch;
     int menu_index=0;
@@ -260,49 +379,3 @@ void menu_input(void)      // This should get passed the user string as a parame
     } // end while
 } // end menu input
 
-void menu1_input(void)
-{
-    char ch, user[30];
-    int menu_index=0;
-    
-    /*
-                The Login menu
-        */
-    menu1(menu_index);
-    while(1)
-    {
-        if(kbhit())
-        {
-            /*
-                                We take in the input and check if it's either UP or DOWN arrow key
-                                If it's one of them then we increment/decrement
-                        */
-            ch=getch();
-            if(ch==72 && menu_index > -1)  // UP key
-            {
-                menu_index--;
-            }
-            else if(ch==80 && menu_index < 3)  // else if DOWN key
-            {
-                menu_index++;
-            }
-            
-            if(menu_index > 2)
-            {
-                menu_index=0;
-            }
-            else if(menu_index < 0)
-            {
-                menu_index=2;
-            }
-            
-            // If the user pressed enter
-            if(ch == 13) 
-            {
-                break;
-            }
-            menu1(menu_index);
-            Sleep(40);
-        } // end kbhit()
-    } // end while() 
-} // end menu1_input
